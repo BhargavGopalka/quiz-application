530753260ea0bca4508437c8f60e5faa040172af
Bhargav Gopalka <bhargav.smartsense@gmail.com>
2018-06-19 16:07:01 +0530

    multi-answers question type completed

--- src/app/user/review/review.component.ts
+++ src/app/user/review/review.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SharedService} from '../../utility/shared-services/shared.service';
 import {RouteConstants} from '../../utility/constants/routes';
 import {Router} from '@angular/router';
+import {QuestionType} from '../../utility/constants/base-constants';
 
 @Component({
   selector: 'app-review',
@@ -9,6 +10,9 @@ import {Router} from '@angular/router';
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent implements OnInit, OnDestroy {
+
+  // Constant variables
+  questionType = QuestionType;
 
   // Data related variable
   answerArraySubscriber: any;
@@ -26,6 +30,7 @@ export class ReviewComponent implements OnInit, OnDestroy {
       this.answerArray = response;
     });
     this.getFinalPoints();
+    console.log(this.answerArray);
   }
 
   // Initialization methods
@@ -38,6 +43,53 @@ export class ReviewComponent implements OnInit, OnDestroy {
   }
 
   // Page events
+  getAnswerStatus(quiz: any) {
+    let isRightAnswer = true;
+    if ((quiz['quizObj']['questionType'] === QuestionType.TRUE_FALSE) ||
+      (quiz['quizObj']['questionType'] === QuestionType.MULTIPLE_CHOICE)) {
+      isRightAnswer = quiz['selectedOption']['isAnswer'];
+    } else if ((quiz['quizObj']['questionType'] === QuestionType.MULTIPLE_ANSWER_SELECTION)) {
+      const selectedOptions = quiz['selectedOption'];
+      const optionsArray = quiz['quizObj']['options'];
+      if (selectedOptions['length'] > 0) {
+
+        /* For loop to check if all the selected option are true/right or not */
+        for (let i = 0; i < selectedOptions['length']; i++) {
+          if (selectedOptions[i]['isAnswer'] === false) {
+            isRightAnswer = false;
+            break;
+          }
+        }
+
+        /* To check if true/right option are included inside the selectedOption array */
+        optionsArray.forEach((option) => {
+          if (option['isAnswer'] === true) {
+            const index = selectedOptions.findIndex((answerOption) => {
+              return answerOption['optionId'] === option['optionId'];
+            });
+            if (index === -1) {
+              isRightAnswer = false;
+            }
+          }
+        });
+      }
+    }
+    return isRightAnswer;
+  }
+
+  getAttemptStatus(quiz: any) {
+    let isAttempted = false;
+    if ((quiz['quizObj']['questionType'] === QuestionType.TRUE_FALSE) ||
+      (quiz['quizObj']['questionType'] === QuestionType.MULTIPLE_CHOICE)) {
+      isAttempted = !!(quiz['selectedOption']);
+    } else if ((quiz['quizObj']['questionType'] === QuestionType.MULTIPLE_ANSWER_SELECTION)) {
+      if (quiz['selectedOption']['length'] > 0) {
+        isAttempted = true;
+      }
+    }
+    return isAttempted;
+  }
+
   onClickQuiz() {
     this._router.navigate(['/' + RouteConstants.QUIZ]);
   }
