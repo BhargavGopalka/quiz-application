74496ce0b18d4ac0bc3dd7a19de4cd3af5181e80
Bhargav Gopalka <bhargav.smartsense@gmail.com>
2018-06-28 18:18:58 +0530

    multiple-choice question type changes

--- src/app/user/components/multiple-choice/multiple-choice.component.ts
+++ src/app/user/components/multiple-choice/multiple-choice.component.ts
@@ -21,22 +21,16 @@ export class MultipleChoiceComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.previousSelection = this.quiz['selectedOption'];
-  }
-
-  /* In the single option and true/false question type, getting status if particular option is previously selected */
-  checkSelectedOption(option: any) {
-    let isSelected = false;
-    if (this.previousSelection && (this.previousSelection['optionId'] === option['optionId'])) {
-      isSelected = true;
+    if (this.previousSelection) {
+      this.selection.setValue(this.previousSelection);
     }
-    return isSelected;
   }
 
   ngOnDestroy() {
     const params = {
       quizObj: this.quiz['quizObj'],
       isMarked: this.quiz['isMarked'],
-      selectedOption: (this.selection.value || this.previousSelection),
+      selectedOption: this.selection.value,
       isNotAttempted: true
     };
     this.updatedAnswerObject.emit(params);
