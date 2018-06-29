9eeb2170c5d6d3f149afab671c8e08be3f1c120c
Bhargav Gopalka <bhargav.smartsense@gmail.com>
2018-06-29 18:40:23 +0530

    changes

--- src/app/user/components/multi-choice-grid/multi-choice-grid.component.ts
+++ src/app/user/components/multi-choice-grid/multi-choice-grid.component.ts
@@ -59,11 +59,9 @@ export class MultiChoiceGridComponent implements OnInit, OnDestroy {
       if (keys.indexOf(i) >= 0) {
         continue;
       }
-
       if (!Object.prototype.hasOwnProperty.call(obj, i)) {
         continue;
       }
-
       target[i] = obj[i];
     }
     return target;
